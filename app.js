d022ce1a5829e37d2d7c10a4783bc93874611b06
EricN <eric_nakagawa@yahoo.com>
2022-04-04 18:08:43 -0700

    Seclection Statements exercise done

--- app.js
+++ app.js
@@ -1,7 +1,40 @@
 console.log("Hello World!\n==========\n");
 console.log("Follow the steps in the README.md file to complete the exercises:\n==========\n");
 
-// Exercise 1
 
 
-// Exercise 2
+
+let num = 5;
+
+let userNum = prompt("Pick a number between 1 - 25");
+
+
+if (num > userNum){
+    alert("Your number is too low!");
+} else if (num < userNum){
+    alert("Your number is too high!");
+} else {
+    alert("You are correct!!");
+}
+
+
+// exercise 2
+
+let userFaveSport = prompt("What is your favorite sport? Basketball, Football or Baseball");
+
+switch (userFaveSport){
+    case "Basketball":
+        alert ("Excellent choice");
+        break;
+    case "Football":
+        alert ("Love It!");
+        break;
+    case "Baseball":
+        alert ("BOOOORINGG!");
+        break;
+        default:
+            alert("Please begin with a capital letter")
+
+}
+
+
